dfcb6c19b5add8c7a3e1fe04d4138935ae4efbbd
THE PUTRAS Media <149865892+theputras@users.noreply.github.com>
2026-02-18 11:23:44 +0700

    Update seed_gate_user.py

--- seed_gate_user.py
+++ seed_gate_user.py
@@ -19,8 +19,8 @@ cipher = Fernet(key)
 TARGET_USER_ID = 1 
 
 # Kredensial Gate/Sicyca yang mau disimpan
-gate_username_input = os.getenv("GATE_ENCRYPTION_KEY")  # Ganti dengan NIM asli
-gate_password_asli  = os.getenv("GATE_ENCRYPTION_KEY") # Ganti dengan Pass asli
+gate_username_input = os.getenv("SICYCA_USER")  # Ganti dengan NIM asli
+gate_password_asli  = os.getenv("SICYCA_PASS") # Ganti dengan Pass asli
 # ========================
 
 # 2. Enkripsi Password
